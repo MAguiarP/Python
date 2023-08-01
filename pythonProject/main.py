0d0e04d91afb17f5888204b2acad1813b54c361c
Matheus de Aguiar <matheusdeaguiar@live.com>
2023-08-01 14:02:58 -0300

    adds

---
+++ pythonProject/main.py
@@ -0,0 +1,22 @@
+print(5 + 5)
+
+numero = 5
+
+print(numero)
+
+print(type(numero))
+
+print("Valores float")
+
+print(1.3 + 2)
+
+num = 4.54
+
+print(num + 0.1)
+
+print(type(num))
+
+conversao = int(num)
+
+print(conversao, type(conversao))
+
