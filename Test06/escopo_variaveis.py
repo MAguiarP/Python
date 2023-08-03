443e4920072ad8bad36d296431de7a857318e6f0
Matheus de Aguiar <matheusdeaguiar@live.com>
2023-08-03 11:38:12 -0300

    add

---
+++ Test06/escopo_variaveis.py
@@ -0,0 +1,12 @@
+funcionario = 'João'
+
+print(funcionario,type(funcionario))
+
+funcionario = 25
+
+print(funcionario,type(funcionario))
+
+if funcionario == 'João':
+    verifica = 'sim'
+    print(verifica, funcionario)
+    
