0b76c09b5a5e52a125c9a2bed7e5b0ebfd106cd4
Matheus de Aguiar <matheusdeaguiar@live.com>
2023-08-07 11:25:08 -0300

    tipos

---
+++ TiposdeDados/main.py
@@ -0,0 +1,12 @@
+nomestring = 'nome'
+
+numeroint = 10
+
+numeroflutuante = 0.333
+
+valoreseoperadoresbooleanos = True, False
+
+valoresoperadoreslogicos = and, or, not
+
+expressoeslogicas = 2 + 3 * 4
+
