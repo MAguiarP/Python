23e671f487a765c57c6f65849207e7f9f8d950cd
Matheus de Aguiar <matheusdeaguiar@live.com>
2023-08-08 17:10:23 -0300

    add

---
+++ test07/adicao.py
@@ -0,0 +1,6 @@
+ex1 = 'João'
+
+ex2 = 12
+
+print(ex2 + 1, ex1)
+
---
+++ test07/operadores.py
@@ -0,0 +1,20 @@
+num1 = 5
+
+num2 = 2
+
+print(num1 - num2)
+
+print(num1 * num2)
+
+print(num1 / num2)
+
+print(num1 ** num2)
+
+print(num1 // num2)
+
+print(num1 % num2)
+
+print('\n\n')
+
+
+print((2+1) * 3)
