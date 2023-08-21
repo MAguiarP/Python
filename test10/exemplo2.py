073372af4c8e4165f777ff05774120b3768c8c8e
Matheus de Aguiar <matheusdeaguiar@live.com>
2023-08-21 16:05:46 -0300

    add

---
+++ test10/exemplo2.py
@@ -0,0 +1,5 @@
+for c in range(1, 6):
+    nome = input(f"Informe o {c} nome: ")
+    print(f"{c}º nome é: ", nome, "\n")
+
+  brea
--- test10/loop_for.py
+++ test10/loop_for.py
@@ -2,3 +2,13 @@ passos = input("Informe quantos passos até o objetivo: ")
 
 for controlador in range(1, int(passos)+1):
     print(f'Andar {controlador} passos')
+
+print('\n')
+
+for numero in range(10):
+    print(numero)
+
+print('\n')
+
+for numero in range(0,11, 10): #valorinicio, valorparada,incremento
+    print(numero)
