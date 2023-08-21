6571de926d4f1a34d4c708a960aa75c02506ef84
Matheus de Aguiar <matheusdeaguiar@live.com>
2023-08-21 10:19:18 -0300

    add

--- test08/if_elif_else.py
+++ test08/if_elif_else.py
@@ -1,5 +1,12 @@
-if ind(idade) > 18:
+idade = input("Informe sua idade: ")
+
+if int(idade) > 18:
     print("Maior de idade")
 
 elif int(idade) < 18:
     print("Menor de idade")
+
+else :
+    print("Tem 18 anos")
+
+    #Estrutura Condicional
