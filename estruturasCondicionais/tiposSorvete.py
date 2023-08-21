bc0fd609cd3c0fe764a9c9e1c39bcddf21a04178
Matheus de Aguiar <matheusdeaguiar@live.com>
2023-08-21 10:52:43 -0300

    add

---
+++ estruturasCondicionais/tiposSorvete.py
@@ -0,0 +1,13 @@
+tipoSorvete = input("Qual o tipo de sorvete? ")
+
+if tipoSorvete == "fruta de Fruta":
+    print("Este é um sorvete de Fruta.")
+
+elif tipoSorvete == "sorvete de Leite":
+    print("Este é um sorvete de Leite.")
+
+elif tipoSorvete == "sorvete de Palito":
+    print("Este é um sorvete de Palito")
+
+else:
+    print("Este não é um sorvete válido.")
