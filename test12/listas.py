11b5c3bc599818ed910a9a37caa6c21716d7c323
Matheus de Aguiar <matheusdeaguiar@live.com>
2023-10-02 15:11:15 -0300

    io

---
+++ test12/listas.py
@@ -0,0 +1,15 @@
+lista1 = [1, 2, 11, 66, 71, 24, 72, 1, 4]
+lista2 = ['p', 'y', 't', 'h', 'o', 'n']
+lista3 = [1, 2, 'a', 'b']
+lista4 = []
+lista5 = list(range(10, -1, -1))
+lista6 = list("Curso de Python")
+
+#ordenar uma lista 
+lista1.sort()
+print(lista1)   
+print(lista2)
+print(lista3)
+print(lista4)
+print(lista5)
+print(lista6)
