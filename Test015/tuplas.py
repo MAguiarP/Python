508e1e459957d5a49212f2c2dd198df4463f3847
Matheus de Aguiar <matheusdeaguiar@live.com>
2023-10-09 13:23:16 -0300

    Create tuplas.py

---
+++ Test015/tuplas.py
@@ -0,0 +1,14 @@
+tupla1 = tuple(range(10))
+print(tupla1)
+
+tupla2 = ('Teste Python', 'Teste interativo') 
+testePython, tipoInterativo = tupla2
+print(testePython, tipoInterativo)
+
+print(sum(tupla1))
+print(max(tupla1))
+print(min(tupla1))
+print(len(tupla1))
+
+for indice, valor in enumerate(tupla1):
+    print(indice, valor)
